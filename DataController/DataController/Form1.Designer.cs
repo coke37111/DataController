8e0c5c5526470ac6db0ca1eae01668b3aebcb395
minsu.kim <coke3711@naver.com>
2022-05-31 00:49:58 +0900

    데이터 툴

---
+++ DataController/DataController/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DataController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileListView = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FileListView
+            // 
+            this.FileListView.AllowDrop = true;
+            this.FileListView.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FileListView.FullRowSelect = true;
+            this.FileListView.GridLines = true;
+            this.FileListView.HideSelection = false;
+            this.FileListView.Location = new System.Drawing.Point(12, 38);
+            this.FileListView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FileListView.Name = "FileListView";
+            this.FileListView.Size = new System.Drawing.Size(250, 654);
+            this.FileListView.TabIndex = 36;
+            this.FileListView.UseCompatibleStateImageBehavior = false;
+            this.FileListView.View = System.Windows.Forms.View.Details;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(9, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 17);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "파일 목록";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 703);
+            this.Controls.Add(this.FileListView);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "DataController V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView FileListView;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DataController/DataController/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataController
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitLayerList();
+        }
+
+        private void InitLayerList()
+        {
+            FileListView.Clear();
+            FileListView.InsertionMark.Color = Color.Blue;
+            FileListView.Columns.Add("File", 245);
+        }
+    }
+}
