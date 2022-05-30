8fd88a721a47c29bff35316ea6ca1d9a3594d9c5
minsu.kim <coke3711@naver.com>
2022-05-31 02:37:46 +0900

    데이터 툴

--- DataController/DataController/Form1.Designer.cs
+++ DataController/DataController/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.FileListView = new System.Windows.Forms.ListView();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // FileListView
@@ -57,11 +58,22 @@
             this.label4.TabIndex = 35;
             this.label4.Text = "파일 목록";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.Location = new System.Drawing.Point(1009, 657);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 35);
+            this.button1.TabIndex = 37;
+            this.button1.Text = "파일 추가";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1270, 703);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.FileListView);
             this.Controls.Add(this.label4);
             this.Name = "Form1";
@@ -76,6 +88,7 @@
 
         private System.Windows.Forms.ListView FileListView;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
